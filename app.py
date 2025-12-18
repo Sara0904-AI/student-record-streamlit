85ef233a169622a94e7da0123b83f4e05dfb9cd8
Sara Yasmin <yasminsara047@gmail.com>
2025-12-18 18:47:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-#---------LOGIN--------
+
 if"logged_in" not in st.session_state:
     st.session_state.logged_in=False
 
@@ -21,7 +21,7 @@ def login_page():
         else:
             st.error("Invalid username and password")
 
-# -------- STUDENT --------
+
 class Student:
     def __init__(self, roll, name, marks):
         self.roll = roll
@@ -29,14 +29,14 @@ class Student:
         self.marks = marks
 
 
-# -------- DISPLAY FUNCTION --------
+
 def display(students):
     st.subheader("Student Records")
     for s in students:
         st.write(f"Roll No: {s.roll} | Name: {s.name} | Marks: {s.marks}")
 
 
-# -------- MARKS --------
+
 def bubble_sort_by_marks(students):
     n = len(students)
     for i in range(n - 1):
@@ -45,7 +45,7 @@ def bubble_sort_by_marks(students):
                 students[j], students[j + 1] = students[j + 1], students[j]
 
 
-# -------- NAME --------
+
 def bubble_sort_by_name(students):
     n = len(students)
     for i in range(n - 1):
@@ -54,7 +54,7 @@ def bubble_sort_by_name(students):
                 students[j], students[j + 1] = students[j + 1], students[j]
 
 
-# -------- ROLL --------
+
 def bubble_sort_by_roll(students):
     n = len(students)
     for i in range(n - 1):
@@ -63,7 +63,7 @@ def bubble_sort_by_roll(students):
                 students[j], students[j + 1] = students[j + 1], students[j]
 
 
-# -------- SEARCH STUDENT --------
+
 def search_student(students, roll_no):
     for s in students:
         if s.roll == roll_no:
@@ -73,13 +73,13 @@ def search_student(students, roll_no):
             st.write(f"Marks: {s.marks}")
             return
     st.error("Student Not Found")
-#--------------------------------------------------
+
 
 if not st.session_state.logged_in:
     login_page()
     st.stop()
 
-#---------MAIN APP-------------------
+
 st.title("🎓 Student Record Management System")
 
 
@@ -98,7 +98,7 @@ menu = st.sidebar.selectbox(
     ),
 )
 
-# -------- ADD STUDENT --------
+
 if menu == "Add Student":
     st.subheader("Add Student")
 
@@ -110,33 +110,33 @@ if menu == "Add Student":
         st.session_state.students.append(Student(roll, name, marks))
         st.success("Student Added Successfully!")
 
-# -------- DISPLAY --------
+
 elif menu == "Display Students":
     if not st.session_state.students:
         st.warning("No students available")
     else:
         display(st.session_state.students)
 
-# -------- MARKS --------
+
 elif menu == "Sort by Marks":
     bubble_sort_by_marks(st.session_state.students)
     st.success("Sorted by Marks")
     display(st.session_state.students)
 
-# -------- NAME --------
 elif menu == "Sort by Name":
     bubble_sort_by_name(st.session_state.students)
     st.success("Sorted by Name")
     display(st.session_state.students)
 
-# --------ROLL --------
+
 elif menu == "Sort by Roll No":
     bubble_sort_by_roll(st.session_state.students)
     st.success("Sorted by Roll No")
     display(st.session_state.students)
 
-# -------- SEARCH --------
+
 elif menu == "Search Student":
     roll_no = st.number_input("Enter Roll No to Search", min_value=1, step=1)
     if st.button("Search"):
         search_student(st.session_state.students, roll_no)
+
